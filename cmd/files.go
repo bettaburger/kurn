bb969a8936858fb6048e3ef1ae25e5257c0ccc17
Stephanie <stephanieha2021@gmail.com>
2026-06-15 21:41:57 -0400

    list and delete cobra commands

--- cmd/files.go
+++ cmd/files.go
@@ -8,67 +8,60 @@ import (
 	"strconv"
 )
 
-var saved string
-var delete string
+var list bool	// list history
+var delete bool // delete a file from history
 
-// FileSaved describes saved command
-var FileSaved = &cobra.Command {
-	Use: "saved",
-	Short: "display saved pcap files",
+
+// FilesCmd describes files command
+var FilesCmd = &cobra.Command {
+	Use: "files",
+	Short: "display previous files",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// show saved files from db
-		db, err := sql.Open("sqlite3", "pcap.db") 
-		if err != nil {
-			return err
-		}
-		defer db.Close()
-		files, err := lite.ListSavedFiles(db)
-		if err != nil {
-			fmt.Println("saved files error: ", err)
-		}
-		for _, f := range files {
-			fmt.Println(f)
+		switch {
+		case list:
+			db, err := sql.Open("sqlite3", "pcap.db") 
+			if err != nil {
+				return err
+			}
+			defer db.Close()
+			// call list files
+			files, err := lite.ListSavedFiles(db)
+			if err != nil {
+				fmt.Println("filescmd error: ", err)
+			}
+			for _, f := range files {
+				fmt.Println(f)
+			}
+		
+		case delete:
+			id, err := strconv.Atoi(args[0])
+			if err != nil {
+				return fmt.Errorf("must be an integer (1...n) %v", err) 
+			}
+			db, err := sql.Open("sqlite3", "pcap.db")
+			if err != nil {
+				return err 
+			}
+			defer db.Close() 
+			// call delete function 
+			rows, err := lite.DelPFile(db, id)
+			if err != nil {
+				return fmt.Errorf("failed to delete file %d, %v", id, err)
+			}
+			if rows == 0 {
+				fmt.Printf("no row found with id %d\n try ./kurn files -l for full list \n", id)
+				return nil
+			}
+			fmt.Printf("deleted %d\n", id)
 		}
 		return nil
 	},
 }
 
-// DeleteFile describes delete file command
-var DeleteFile = &cobra.Command {
-	Use: "delete",
-	Short: "delete",
-	Args: cobra.ExactArgs(1),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// run del function
-		id, err := strconv.Atoi(args[0])
-		if err != nil {
-			return fmt.Errorf("must be an integer (1...n) %v", err) 
-		}
-		db, err := sql.Open("sqlite3", "pcap.db")
-		if err != nil {
-			return err 
-		}
-		defer db.Close() 
-		rows, err := lite.DelPFile(db, id)
-		if err != nil {
-			return fmt.Errorf("failed to delete file %d, %v", id, err)
-		}
-		if rows == 0 {
-			fmt.Printf("no row found with id %d\n try ./kurn saved for full list \n", id)
-			return nil
-		}
-		fmt.Printf("deleted %d\n", id)
-		return nil 
-	},
-}
 func init() {
-	rootCmd.AddCommand(FileSaved)
-	rootCmd.AddCommand(DeleteFile)
-	FileSaved.Flags().StringVarP(&saved, "saved", "s", "SAVED", "display saved pcap files") // ./kurn saved
-
-	DeleteFile.Flags().StringVarP(&delete, "delete", "d", "DELETE", "delete a pcap file from saved") // ./kurn delete <id>
-
-
-
+	rootCmd.AddCommand(FilesCmd)
+	FilesCmd.Flags().BoolVarP(&list, "list",  "l", false, "display history of pcap files") // ./kurn files -l
+	FilesCmd.Flags().BoolVarP(&delete, "delete", "d", false, "delete a pcap file from history") // ./kurn files -d <id>
 }
 
--- internal/server/lite/pcap.go
+++ internal/server/lite/pcap.go
@@ -50,41 +50,6 @@ func InsertPFile(db *sql.DB, pf PFile) (int64, error) {
 	return id, nil // return new album id
 }
 
-// delete pcap file from pcap.db by ID number 
-func DelPFile(db *sql.DB, id int) (int64, error){
-	delQ := `DELETE FROM pcapfiles WHERE id = ?`
-	row, err := db.Exec(delQ, id)
-	if err != nil {
-		return 0, fmt.Errorf("del pcapfile: %v", err)
-	}
-	return ReturnRows(row)
-}
-
-// returns number of rows affected by an update, insert or delete
-func ReturnRows(row sql.Result) (int64, error) { return row.RowsAffected() }
-
-// list current queries in db
-// next -> filter queries via id, pf, path, size or hash
-func ListSavedFiles(db *sql.DB) ([]PFile, error) {
-	listQ := `SELECT * FROM pcapfiles`
-	rows, err := db.Query(listQ)
-	if err != nil {
-		return nil, err
-	}
-	// hold files
-	var files []PFile
-	for rows.Next() {
-		var pf PFile
-		if err := rows.Scan(&pf.id, &pf.Filename, &pf.Path, &pf.Size, &pf.Hash256); err != nil {
-			return files, err
-		}
-		files = append(files, pf)
-	}
-	if err = rows.Err(); err != nil {
-		return files, err 
-	}
-	return files, nil
-}
 
 // hash packet contents
 func CreateSHA256HashFile(filepath string) string {
